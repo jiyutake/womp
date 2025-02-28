44542378174115e0a7eb1448dd9cdcd2a507d666
jiyutake <tiffany254@proton.me>
2025-02-28 21:22:28 +0000

    many such news

--- scripts/news.py
+++ scripts/news.py
@@ -8,15 +8,15 @@ lines = open("./assets/newsline.txt", "r").read().strip().split("\n")
 
 while True: 
     if getoutput("./bin/performancevar.sh get news").strip() == "false":
-        time.sleep(60)
+        time.sleep(5)
         continue
     line = random.choice(lines)
     print(line, flush=True)
     time.sleep(2)
     for i in range(len(line)): 
         print(line[i:], flush=True)
-        time.sleep(0.12)
+        time.sleep(0.1)
     print(flush=True)
-    time.sleep(random.randint(30, 90))
+    time.sleep(random.randint(3, 9))
 
 
