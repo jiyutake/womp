c65788e4069e0b822d5be0925df930c90f39fad5
FailedEx <literallyasalmon@gmail.com>
2025-03-11 23:19:13 +1300

    float bar

--- bin/start.sh
+++ bin/start.sh
@@ -10,11 +10,14 @@ bspc config active_border_color $(echo $THEME | jq -r  '."highlight"')
 bspc rule -a Eww border=off state=floating
 bspc rule -a zenity border=off state=floating
 
-if pidof picom; then 
-    eww -c $EWW_DIR open-many bar keepopen screenborder
-else
-    eww -c $EWW_DIR open-many bar keepopen
-fi
+# leave gap
+bspc config -m ^1 left_padding 60px
+
+# if pidof picom; then 
+#     eww -c $EWW_DIR open-many bar keepopen screenborder
+# else
+#     eww -c $EWW_DIR open-many bar keepopen
+# fi
+eww -c $EWW_DIR open-many bar keepopen
 
 $EWW_DIR/bin/wallset.sh restore
-
