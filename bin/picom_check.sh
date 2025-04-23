b3e23e8eaf2a7d3cc14178e1531ca260b0b713c1
FailedEx <literallyasalmon@gmail.com>
2025-04-23 17:28:06 +1200

    Fixed compositor toggle

--- bin/picom_check.sh
+++ bin/picom_check.sh
@@ -2,4 +2,6 @@
 
 EWW_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.."
 
-eww -c $EWW_DIR reload
+# unironically works better to just kill and reopen womp
+killall eww
+$EWW_DIR/bin/start.sh &
